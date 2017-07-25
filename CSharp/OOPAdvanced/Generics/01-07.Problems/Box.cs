f23986b984e4eda86d40e85fd70811fa06a27ac4
Kamen <kamenminkov.com@abv.bg>
2017-07-25 14:23:10 +0300

    Generics first 7 problems

---
+++ CSharp/OOPAdvanced/Generics/01-07.Problems/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Box<T> : IComparable
+{
+    public Box(T value)
+    {
+        this.Value = value;
+    }
+
+    public T Value { get; set; }
+
+    public override string ToString() =>  Value.GetType().FullName + ": " + Value;
+
+    public int CompareTo(object obj)
+    {
+        return Comparer<T>.Default.Compare(Value, (T)obj);
+    }
+}
+
---
+++ CSharp/OOPAdvanced/Generics/01-07.Problems/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class StartUp
+{
+    static void Main()
+    {
+        //it is easy to change the type of a comparison
+        //just change the type of the input you would like to receive
+        //e.g.: here you can change double with string or decimal
+        int n = int.Parse(Console.ReadLine());
+        List<Box<double>> list = new List<Box<double>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            list.Add(new Box<double>(double.Parse(input)));
+        }
+        double comparison = double.Parse(Console.ReadLine());
+        Console.WriteLine(GreatererCount(list, comparison));   
+    }
+
+    public static void Swap<T>(List<T> list, int index1, int index2)
+    {
+        var help = list[index1];
+        list[index1] = list[index2];
+        list[index2] = help;
+    }
+            
+    public static int GreatererCount<T, M>(List<T> list, M element)
+        where T : IComparable
+    {
+       return list.Count(x => x.CompareTo(element) > 0);
+    }
+}
+
