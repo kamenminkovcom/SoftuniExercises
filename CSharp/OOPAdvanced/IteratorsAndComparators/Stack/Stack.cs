52aea72edfd23706b1a09b8b963dee647d8b4ec0
Kamen <kamenminkov.com@abv.bg>
2017-07-30 01:29:57 +0300

    Stack

---
+++ CSharp/OOPAdvanced/IteratorsAndComparators/Stack/Stack.cs
@@ -0,0 +1,50 @@
+﻿namespace Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Stack<T> : IEnumerable<T>
+    {
+
+        private List<T> storage;
+
+        public Stack()
+        {
+            storage = new List<T>();
+        }
+
+        public Stack(List<T> initialElements)
+        {
+            storage = initialElements;
+        }
+
+        public void Push(T element)
+        {
+            storage.Add(element);
+        }
+
+        public T Pop()
+        {
+            int stackCount = storage.Count;
+            if (stackCount == 0)
+                throw new InvalidOperationException();
+            T element = storage[stackCount - 1];
+            storage.RemoveAt(stackCount - 1);
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = storage.Count - 1; i >= 0; i--)
+            {
+                yield return storage[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ CSharp/OOPAdvanced/IteratorsAndComparators/Stack/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Stack
+{
+    class StartUp
+    {
+        public static Stack<int> stack = new Stack<int>();
+
+        static void Main()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                    break;
+
+                List<string> elements = input.Split(new[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (elements[0] == "Push")
+                {
+                    Push(elements);
+                    continue;
+                }
+                try
+                {
+                    stack.Pop();
+                }
+                catch
+                {
+                    Console.WriteLine("No elements");
+                }
+            }
+
+            for (int i = 0; i <= 1; i++)
+            {
+                foreach (var element in stack)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+        }
+
+        public static void Push(List<string> input)
+        {
+            input
+                .RemoveAt(0);
+            input
+                .Select(int.Parse)
+                .ToList()
+                .ForEach(x => stack.Push(x));
+        }
+    }
+}
