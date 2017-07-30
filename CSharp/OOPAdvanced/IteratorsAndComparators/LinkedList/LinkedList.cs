32d7be0326c406d78cfd1fcde3a743a9cd97ea15
Kamen <kamenminkov.com@abv.bg>
2017-07-30 14:59:20 +0300

    Linked list

---
+++ CSharp/OOPAdvanced/IteratorsAndComparators/LinkedList/LinkedList.cs
@@ -0,0 +1,90 @@
+﻿namespace LinkedList
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class LinkedList<T> : IEnumerable<T>
+        where T : IComparable<T>
+    {
+        private class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+            public Node NextNode { get; set; }
+            public T Value { get; }
+        }
+
+        private Node firstNode;
+        private Node lastNode;
+
+        public int Count { get; private set; }
+
+        public LinkedList()
+        {
+            Count = 0;
+        }
+
+        public void Add(T value)
+        {
+            Node element = new Node(value);
+            if (Count == 0)
+            {
+                firstNode = element;
+                lastNode = element;
+            }
+            else
+            {
+                lastNode.NextNode = element;
+                lastNode = lastNode.NextNode;
+            }
+            Count++;
+        }
+
+        public bool Remove(T value)
+        {
+            if (Count == 0)
+                throw new InvalidOperationException();
+            Node prevNode = null;
+            Node currentNode = firstNode;
+            while (currentNode != null)
+            {
+                if (currentNode.Value.CompareTo(value) == 0)
+                {
+                    if (prevNode == null)
+                    {
+                        firstNode = currentNode.NextNode;
+                    }
+                    else
+                    {
+                        prevNode.NextNode = currentNode.NextNode;
+                    }
+                    Count--;
+                    return true;
+                }
+                prevNode = currentNode;
+                currentNode = currentNode.NextNode;
+            }
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var currentElement = firstNode;
+            while (currentElement != null)
+            {
+                yield return currentElement.Value;
+                currentElement = currentElement.NextNode;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString() => string.Join(" ", this);
+    }
+}
---
+++ CSharp/OOPAdvanced/IteratorsAndComparators/LinkedList/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using System.Security;
+
+namespace LinkedList
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int operationsNumber = int.Parse(Console.ReadLine());
+            LinkedList<int> linkedList = new LinkedList<int>();
+            for (int i = 0; i < operationsNumber; i++)
+            {
+                string[] operation = Console.ReadLine().Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries);
+                Call(linkedList, operation[0], int.Parse(operation[1]));
+            }
+
+            Console.WriteLine(linkedList.Count);
+            Console.WriteLine(linkedList.ToString());
+        }
+
+        private static void Call<T>(LinkedList<T> list, string method, int param)
+            where T : IComparable<T>
+        {
+            Type type = typeof(LinkedList<T>);
+            MethodInfo methodInfo = type.GetMethod(method);
+            methodInfo.Invoke(list, new object[] {param});
+        }
+    }
+}
