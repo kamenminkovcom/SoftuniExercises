a97317089d961ace8004903ed82d888f705254c0
Kamen <kamenminkov.com@abv.bg>
2017-08-05 19:10:40 +0300

    Reflection

---
+++ CSharp/OOPAdvanced/Reflection/01HarestingFields/HarvestingFieldsTest.cs
@@ -0,0 +1,50 @@
+﻿namespace _01HarestingFields
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    class HarvestingFieldsTest
+    {
+        static void Main()
+        {
+            Type harvestType = typeof(HarvestingFields);
+            var fileds = harvestType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "HARVEST")
+                {
+                    return;
+                }
+
+                switch (input)
+                {
+                    case "protected":
+                        GetFields(fileds.Where(x => x.IsFamily));
+                        break;
+                    case "public":
+                        GetFields(fileds.Where(x => x.IsPublic));
+                        break;
+                    case "private":
+                        GetFields(fileds.Where(x => x.IsPrivate));
+                        break;
+                    case "all":
+                        GetFields(fileds);
+                        break;
+                }
+            }
+        }
+
+        public static void GetFields(IEnumerable<FieldInfo> fields)
+        {
+            foreach (var field in fields)
+            {
+                Console.WriteLine($"{field.Attributes.ToString().ToLower()} {field.FieldType.Name} {field.Name}".Replace("family", "protected"));
+            }
+        }
+    }
+}
---
+++ CSharp/OOPAdvanced/Reflection/02BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+
+namespace _02BlackBoxInteger
+{
+    using System;
+
+    class BlackBoxIntegerTests
+    {
+        static void Main()
+        {
+            Type[] types = new Type[] { typeof(int) };
+            object[] prameteres = new object[] { 0 };
+
+            var blackBox = CreateInstance<BlackBoxInt>(types, prameteres);
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    return;
+                }
+
+                string[] commands = input.Split(new[] { "_" }, StringSplitOptions.RemoveEmptyEntries);
+
+                Call(blackBox, commands[0], new object[] { int.Parse(commands[1]) });
+
+                Console.WriteLine(GetResult("innerValue", blackBox));
+            }
+        }
+
+        public static T CreateInstance<T>(Type[] paramTypes, object[] parameters)
+        {
+            Type type = typeof(T);
+
+            ConstructorInfo constructor =
+                type.GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, paramTypes, null);
+
+            return (T)constructor.Invoke(parameters);
+        }
+
+        public static void Call<T>(T instance, string methodName, object[] parameters)
+        {
+            MethodInfo method = typeof(T).GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+            method.Invoke(instance, parameters);
+        }
+
+        public static int GetResult<T>(string fieldName, T instance)
+        {
+            return (int)typeof(T).GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance)
+                .GetValue(instance);
+        }
+    }
+}
